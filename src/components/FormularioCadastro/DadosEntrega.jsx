5125b51d268a6d4be7001bd6ebe955c98771a63c
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-09-30 14:43:28 -0300

    Hooks e stepper

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, TextField } from '@material-ui/core';
 
-function DadosEntrega() {
+function DadosEntrega({ aoEnviar }) {
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
+
+  function HandleSubmit(e){
+    e.preventDefault();
+    aoEnviar({cep, endereco, numero, estado, cidade});
+  }
+
+  function handleCep(e){
+    setCep(e.target.value);
+  }
+
+  function handleEndereco(e){
+    setEndereco(e.target.value);
+  }
+
+  function handleNumero(e){
+    setNumero(e.target.value);
+  }
+
+  function handleEstado(e){
+    setEstado(e.target.value);
+  }
+
+  function handleCidade(e){
+    setCidade(e.target.value);
+  }
+
   return (
-    <form>
-      <TextField id="cep" label="CEP" type="number" variant="outlined" margin="normal" />
-      <TextField id="endereco" label="Endereço" type="text" variant="outlined" margin="normal" fullWidth />
-      <TextField id="numero" label="Número" type="number" variant="outlined" margin="normal" />
-      <TextField id="estado" label="Estado" type="number" variant="outlined" margin="normal" />
-      <TextField id="cidade" label="Cidade" type="number" variant="outlined" margin="normal" />
+    <form onSubmit={HandleSubmit}>
+      <TextField value={cep} onChange={handleCep} id="cep" label="CEP" type="number" variant="outlined" margin="normal" />
+      <TextField value={endereco} onChange={handleEndereco} id="endereco" label="Endereço" type="text" variant="outlined" margin="normal" fullWidth />
+      <TextField value={numero} onChange={handleNumero} id="numero" label="Número" type="number" variant="outlined" margin="normal" />
+      <TextField value={estado} onChange={handleEstado} id="estado" label="Estado" type="number" variant="outlined" margin="normal" />
+      <TextField value={cidade} onChange={handleCidade} id="cidade" label="Cidade" type="number" variant="outlined" margin="normal" />
 
       <Button type="submit" variant="contained" color="primary" fullWidth >Finalizar Cadastro</Button>
     </form>
--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,17 +1,27 @@
+import React, { useState } from 'react'
 import { Button, TextField } from '@material-ui/core'
-import React from 'react'
 
 function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
   function handleForm(e){
     e.preventDefault();
-    aoEnviar()
+    aoEnviar({ email, senha })
+  }
+
+  function handleEmail(e){
+    setEmail(e.target.value);
+  }
+
+  function handleSenha(e){
+    setSenha(e.target.value);
   }
 
   return (
     <form onSubmit={handleForm}>
-      <TextField id="email" label="Email" type="email" variant="outlined" margin="normal" fullWidth required />
-      <TextField id="senha" label="Senha" type="password" variant="outlined" margin="normal" fullWidth required />
+      <TextField value={email} onChange={handleEmail} id="email" label="Email" type="email" variant="outlined" margin="normal" fullWidth required />
+      <TextField value={senha} onChange={handleSenha} id="senha" label="Senha" type="password" variant="outlined" margin="normal" fullWidth required />
       <Button type="submit" variant="contained" color="primary" >Cadastrar</Button>
     </form>
   )
