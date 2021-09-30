a51c51d18ad3daff1678b2123b0849352d7d6e40
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-09-30 16:21:18 -0300

    Validacao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { validarCPF, validarSenha } from "./models/cadastro";
 import "fontsource-roboto";
 
 import { Container, Typography } from '@material-ui/core';
@@ -9,7 +10,7 @@ class App extends Component {
     return (
       <Container component="article" maxWidth="sm" >
         <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+        <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha}} />
       </Container>
     );
   }
@@ -17,14 +18,6 @@ class App extends Component {
 
 function aoEnviarForm(dados){
   console.log(dados)
-}
-
-function validarCPF(cpf){
-  if(cpf.length !== 11){
-    return {valido:false, texto: "CPF deve ter 11 digitos"}
-  }else {
-    return {valido:true, texto: ""}
-  }
 }
 
 export default App;
--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { TextField, Button, Switch, FormControlLabel } from '@material-ui/core/'
 
-function DadosPessoais({ aoEnviar, validarCPF }) {
+function DadosPessoais({ aoEnviar, validacoes }) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
@@ -26,9 +26,12 @@ function DadosPessoais({ aoEnviar, validarCPF }) {
     setCpf(e.target.value);
   }
 
-  const handleOnBlurCpf = (e) => {
-    const isValid = validarCPF(cpf);
-    setErros({cpf:isValid});
+  const validarCampos = (e) => {
+    const { name, value } = e.target;
+    const novoEstado = {...erros}
+    novoEstado[name] = validacoes[name](value);
+    setErros(novoEstado);
+    console.log(novoEstado)
   }
 
   const handlePromocoes = (e) => {
@@ -45,7 +48,7 @@ function DadosPessoais({ aoEnviar, validarCPF }) {
 
       <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" value={sobrenome} onChange={handleSobrenome} />
 
-      <TextField id="cpf" label="CPF" variant="outlined" fullWidth margin="normal" value={cpf} onChange={handleCpf} error={!erros.cpf.valido} helperText={erros.cpf.texto} onBlur={handleOnBlurCpf} />
+      <TextField id="cpf" label="CPF" name="cpf" variant="outlined" fullWidth margin="normal" value={cpf} onChange={handleCpf} error={!erros.cpf.valido} helperText={erros.cpf.texto} onBlur={validarCampos} />
 
       <FormControlLabel label="Promocoes" control={<Switch name="Promocoes" defaultChecked color="primary" />} value={promocoes} onChange={handlePromocoes} />
 
