d6781f96e66b7c60ceb17abc636baa3bf8f026e8
Gustavo Santos <gustavo.santos@keyrus.com.br>
2021-10-05 16:12:18 -0300

    Context

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { validarCPF, validarSenha } from "./models/cadastro";
 import "fontsource-roboto";
 
 import { Container, Typography } from '@material-ui/core';
+import ValidacoesCadastro from "./contexts/validacoes";
 
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm" >
         <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha, nome: validarSenha}} />
+        <ValidacoesCadastro.Provider value={{cpf:validarCPF, senha:validarSenha, nome: validarSenha}}>
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
+        </ValidacoesCadastro.Provider>
       </Container>
     );
   }
